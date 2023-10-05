7e0d3c64282939074974a3092a6335c10097d368
Hossam <hossam.sayed@passboard.net>
2023-10-05 22:17:03 +0300

    Setup project structure

--- app/src/main/java/com/devabits/mawaqet/feature_mawaqet/presentation/MainActivity.kt
+++ app/src/main/java/com/devabits/mawaqet/feature_mawaqet/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.devabits.mawaqet
+package com.devabits.mawaqet.feature_mawaqet.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devabits.mawaqet.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
